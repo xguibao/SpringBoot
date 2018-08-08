77a656ad97a9dd20cbb61fc8916d84bfb05efc1c
xugb <865719772@qq.com>
2018-08-08 18:56:31 +0800

    测试提交

--- src/main/java/com/xugb/SpringbootApplication.java
+++ src/main/java/com/xugb/SpringbootApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringbootApplication {
 
     public static void main(String[] args) {
-        System.out.println("");
+        System.out.println("啊");
         SpringApplication.run(SpringbootApplication.class, args);
     }
 }
--- src/main/java/com/xugb/gittest/Main.java
+++ src/main/java/com/xugb/gittest/Main.java
@@ -2,6 +2,6 @@ package com.xugb.gittest;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("上传成功");
+        System.out.println("上传成功啊");
     }
 }
